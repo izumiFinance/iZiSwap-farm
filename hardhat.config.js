2382d5cacab63b0fb8db3ce77ba81c7ccb5424f8
opcc <opcc22059@gmail.com>
2022-06-09 13:58:02 +0800

    update

---
+++ hardhat.config.js
@@ -0,0 +1,84 @@
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-contract-sizer');
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-truffle5");
+// require('hardhat-docgen');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const secret = require('./.secret.js');
+
+const apiKey = secret.pkApiKey;
+const sk = secret.pk;
+const sk2 = secret.pk2;
+const sk3 = secret.pk3;
+const izumiRpcUrl = "http://47.241.103.6:9545";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+
+  networks: {
+    izumiTest: {
+      url: izumiRpcUrl,
+      gas: 8000000,
+      gasPrice: 50000000000,
+      accounts: [sk, sk2, sk3]
+    },
+    arbitrum: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      accounts: [sk]
+    },
+    polygon: {
+      url: 'https://rpc-mainnet.maticvigil.com',
+      accounts: [sk]
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      gas: 10000000,
+      gasPrice: 2500000000,
+      accounts: [sk]
+    },
+    ethereum: {
+      url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      gas: 7792207,
+      gasPrice: 25000000000,
+      accounts: [sk]
+    },
+    bscTest: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      accounts: [sk],
+      // gas: 90000000,
+      gasPrice: 10000000000,
+    },
+  },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
+  },
+  etherscan: {
+    apiKey: apiKey,
+  }
+};
