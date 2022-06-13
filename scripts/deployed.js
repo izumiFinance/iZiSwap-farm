fda169c1710aab5bbccc547219695f7a5b253b7d
opcc <opcc22059@gmail.com>
2022-06-13 21:20:26 +0800

    update

---
+++ scripts/deployed.js
@@ -0,0 +1,12 @@
+const contracts = {
+    bscTest: {
+        liquidityManager: '0x6bEae78975e561fDF27AaC6f09F714E69191DcfD',
+        WBNB: '0xa9754f0D9055d14EB0D2d196E4C51d8B2Ee6f4d3',
+        iZi: '0x551197e6350936976DfFB66B2c3bb15DDB723250',
+        BIT: '0x78EC33d176FC2a957B87444f71b12bf465193Bf5',
+        USDT: '0x6AECfe44225A50895e9EC7ca46377B9397D1Bb5b',
+        USDC: '0x876508837C162aCedcc5dd7721015E83cbb4e339',
+    },
+}
+
+module.exports = contracts;
